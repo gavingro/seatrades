fad58e2a6818417f78d92369a66b178acebd53cc
Gavin Grochowski <grochowskigavin@gmail.com>
2023-02-16 22:09:18 -0800

    Add wrangle longform and preference lookup.

--- src/seatrades.py
+++ src/seatrades.py
@@ -12,7 +12,7 @@ class Seatrades:
 
     def __init__(
         self,
-        camper_prefs: Dict[str, Dict[str, List[str]]],
+        cabin_camper_prefs: Dict[str, Dict[str, List[str]]],
         seatrades_prefs: Dict[str, Dict[str, int]],
     ):
         """
@@ -20,31 +20,43 @@ class Seatrades:
 
         Parameters
         ----------
-        camper_prefs : dict
+        cabin_camper_prefs : dict
             A json-like dict containing the camper-cabin-seatrade
             preferences information.
         seatrades_prefs : dict
             A json-like dict containing the seatrade-minsize-maxsize
             information.
         """
+        # Helper Function
         def flatten(outer_list: List[list]):
             """Flattens the 2d input list into 1d."""
-            return [item for sublist in outer_list for item in sublist]
+            return {
+                key: value
+                for inner_dict in outer_list.values()
+                for key, value in inner_dict.items()
+            }
 
-        self.camper_prefs = camper_prefs
-        self.seatrades_prefs = seatrades_prefs
+        # def flatten_dict(outer_dict: Dict[dict]):
+        #     """Flattens the 2d input dict into 1d."""
+        #     output_dict = {}
+        #     for inner_dict in outer_dict.values():
+        #         for key, value in inner_dict.items():
+        #             output_dict[key] = value
+        #     return output_dict
+
+        self.cabin_camper_prefs = cabin_camper_prefs
+        self.cabins = list(cabin_camper_prefs.keys())
+        self.camper_prefs = flatten(self.cabin_camper_prefs)
+        self.campers = list(self.camper_prefs.keys())
         # Seatrades for block 1 and block 2.
+        self.seatrades_prefs = seatrades_prefs
         self.seatrades = list(seatrades_prefs.keys())
         self.seatrades1 = [f"1_{seatrade}" for seatrade in self.seatrades]
         self.seatrades2 = [f"2_{seatrade}" for seatrade in self.seatrades]
         self.seatrades_full = self.seatrades1 + self.seatrades2
-        self.cabins = list(camper_prefs.keys())
-        self.campers = flatten(
-            [list(cabin.keys()) for cabin in list(camper_prefs.values())]
-        )
         self.assignments: pd.DataFrame
 
-    def assign(self) -> pd.DataFrame:
+    def assign(self) -> int:
         """
         Uses the objects campers_df and seatrades_df to solve a Linear Programming
         problem to assign each camper to their ideal seatrades while respecting
@@ -54,77 +66,85 @@ class Seatrades:
 
         Returns
         -------
-        pd.DataFrame
-            A matrix of binary 1's and 0's (assigned or not assigned) representing
-            the assignment of campers (Y Axis) to seatrades in block 1 or 2 (X Axis).
+        int
+            A status code representing feasibility of the problem.
+            1 if failure, 0 if success.
         """
         # Setup problem and parameters.
         problem = pulp.LpProblem(name="seatrades_assignment")
         assignments = pulp.LpVariable.dicts(
-            "Assignment", 
+            "Assignment",
             (self.campers, self.seatrades_full),
             lowBound=0,
             upBound=1,
-            cat=pulp.LpInteger
+            cat=pulp.LpInteger,
         )
-        
+
         # CONSTRAINTS:
         # Constraint 1: Each camper is assigned 1 seatrade in each of 2 blocks.
         for block_index, seatrades in enumerate([self.seatrades1, self.seatrades2]):
             for c in self.campers:
                 problem += (
                     pulp.lpSum([assignments[c][s] for s in seatrades]) == 1,
-                    f"{c}_in_only_1_seatrade_{block_index}"
+                    f"{c}_in_only_1_seatrade_block_{block_index}",
                 )
-        #Constraint 2: Each seatrade is assigned between min and max campers.
-        for s in self.seatrades_full:
-            seatrade = s[2:] # Remove block index for matching.
-            problem += (
-                pulp.lpSum([assignments[c][s] for c in self.campers]) 
-                >= self.seatrades_prefs[seatrade]["campers_min"],
-                f"More_than_{self.seatrades_prefs[seatrade]['campers_min']}_in_{s}"
-            )
-            problem += (
-                pulp.lpSum([assignments[c][s] for c in self.campers]) 
-                <= self.seatrades_prefs[seatrade]["campers_max"],
-                f"Less_than_{self.seatrades_prefs[seatrade]['campers_max']}_in_{s}"
-            )
-        #Constraint 3: Campers cannot be assigned un-requested seatrades.
-        for campers in self.camper_prefs.values():
-            for c, seatrade_prefs in campers.items():
+        # Constraint 2: Each camper cannot be assigned the same seatrade
+        # in both blocks.
+        for s1, s2 in zip(self.seatrades1, self.seatrades2):
+            for c in self.campers:
                 problem += (
-                    pulp.lpSum([
-                        assignments[c][s] for s in self.seatrades_full 
-                        if s[2:] not in seatrade_prefs  # Remove block index.
-                    ]),
-                    f"{c}_prefers_not_{s}"
+                    pulp.lpSum([assignments[c][s1], assignments[c][s2]]) <= 1,
+                    f"{c} can't take {s1[2:]} in both blocks",
                 )
+        # Constraint 3: Each seatrade is assigned between min and max campers.
+        for s in self.seatrades_full:
+            seatrade = s[2:]  # Remove block index for matching.
+            problem += (
+                pulp.lpSum([assignments[c][s] for c in self.campers])
+                >= self.seatrades_prefs[seatrade]["campers_min"],
+                f"More_than_{self.seatrades_prefs[seatrade]['campers_min']}_in_{s}",
+            )
+            problem += (
+                pulp.lpSum([assignments[c][s] for c in self.campers])
+                <= self.seatrades_prefs[seatrade]["campers_max"],
+                f"Less_than_{self.seatrades_prefs[seatrade]['campers_max']}_in_{s}",
+            )
+        # Constraint 4: Campers cannot be assigned un-requested seatrades.
+        for c, seatrade_prefs in self.camper_prefs.items():
+            problem += (
+                pulp.lpSum(
+                    [
+                        assignments[c][s]
+                        for s in self.seatrades_full
+                        if s[2:] not in seatrade_prefs
+                    ]
+                )
+                == 0,
+                f"{c}_prefers_not_these_seatrades.",
+            )
 
-        #OBJECTIVE:
+        # OBJECTIVE:
         obj = 0
-        #Penalize giving lower-preference seatrades.
-        for cabin, campers in self.camper_prefs.items():
-            for c, preferences in campers.items():
-                for block in [1, 2]:
-                    obj += (
-                        pulp.lpSum(
-                            [
-                                # Use indicator function from assignment 
-                                # multiplied by linear preference penalty
-                                # from index.
-                                assignments[c][f"{block}_{s}"] 
-                                * (preferences.index(s)) 
-                                for s in preferences
-                            ]
-                        )
-                    )
+        # Penalize giving lower-preference seatrades.
+        for c, preferences in self.camper_prefs.items():
+            for block in [1, 2]:
+                obj += pulp.lpSum(
+                    [
+                        # Use indicator function from assignment
+                        # multiplied by linear preference penalty
+                        # from index.
+                        assignments[c][f"{block}_{s}"] * (preferences.index(s) + 1)
+                        for s in preferences
+                    ]
+                )
         problem += obj
-        
-        # Solve:
-        status = problem.solve()
 
+        # Solve and save assignments:
+        print(problem)
+        status = problem.solve()
         self.assignments = pd.DataFrame(assignments).applymap(pulp.value).transpose()
-        return self.assignments
+        self.status = status
+        return self.status
 
     def get_assignments_by_cabin(self, assignments: pd.DataFrame) -> dict:
         """Get the assignments organized by Cabin -> Camper -> Seatrade."""
@@ -141,13 +161,43 @@ class Seatrades:
         Melts the wide-form sparse dataframe into long-form, and adds column
         for camper preference scores of assigned seatrade.
         """
-        ...
+        df = (
+            assignments.melt(
+                var_name="seatrade", ignore_index=False, value_name="assignment"
+            )
+            .reset_index()
+            .rename(columns={"index": "camper"})
+        )
+        # Helper lookup function
+        def lookup_preference(row) -> int:
+            """
+            Returns the preference number of the
+            seatrade for a given camper.
+            If not selected due to infeasibility,
+            returns 999.
+            """
+            if row.assignment:
+                row_camper_prefs = self.camper_prefs[row.camper]
+                if row.seatrade[2:] in row_camper_prefs:
+                    pref_rank = row_camper_prefs.index(row.seatrade[2:])
+                else:
+                    pref_rank = 999
+            else:
+                pref_rank = 0
+            return pref_rank
 
-    def display_assignments(self, assignments: pd.DataFrame) -> alt.Chart:
+        df["preference"] = df.apply(lookup_preference, axis=1)
+        return df
+
+    def display_assignments(self) -> alt.Chart:
         """
         Displays the assignments of the seatrades visually for inference.
         """
-        ...
+        if not self.assigments:
+            raise ValueError(
+                "Seatrades.assignments not found."
+                "Did you remember to run Seatrades.assign() first?"
+            )
 
     def export_assignments_to_csv(self, filepath: str = "assignments.csv"):
         """
@@ -159,4 +209,9 @@ class Seatrades:
         filepath : str
            The str to the filepath to export the assignments to.
         """
-        ...
+        if not self.assigments:
+            raise ValueError(
+                "Seatrades.assignments not found."
+                "Did you remember to run Seatrades.assign() first?"
+            )
+        raise NotImplementedError
